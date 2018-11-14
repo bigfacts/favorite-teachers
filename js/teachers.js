5cf71eb4bfb0abd384956d4df15aa6501b0d8b66
Spencer Daufeldt <spedaufeldt@wl.k12.ia.us>
2018-11-14 14:25:19 -0600

    teachers

---
+++ js/teachers.js
@@ -0,0 +1,35 @@
+var teachers = [
+    {
+        image: "hauschildt.jpg",
+        name: "Marc Hauschildt",
+        subjects: ["codeing class", "yearbook", "business law"],
+        teacher_id: '001',
+    },
+    {
+        image: "hood.jpg",
+        name: "Betsy Hood",
+        subjects: ["child development", "beggening foods", "food and nutrition"],
+        teacher_id: '002',
+    },
+    {
+        image: "harvey.jpg",
+        name: "Sarah Harvey",
+        subjects: ["math 2", "math 3", "topics"],
+        teacher_id: '003',
+    },
+    {
+        image: "czarnecki.jpg",
+        name: "Mike Czarnecki ",
+        subjects: ["gym","strength and conditioning"],
+        teacher_id: '004',
+    },
+    {
+        image: "morris.jpg",
+        name: "Zach Morris",
+        subjects: ["intro to ag","conservation", "ag leadership"],
+        teacher_id: '005',
+    },
+
+
+    
+];
