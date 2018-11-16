f54274069edd9faac2713e08ca92b9db6f3b8b9e
Spencer Daufeldt <spedaufeldt@wl.k12.ia.us>
2018-11-16 14:22:22 -0600

    stars, color, names

--- js/teacher-constructor.js
+++ js/teacher-constructor.js
@@ -6,7 +6,9 @@ var Teacher = function(teacher){
    this.image = teacher.image;
    this.name = teacher.name;
    this.subjects = teacher.subjects;
+   this.stars = teacher.stars;
    this.teacher_id = teacher.teacher_id;
+
    
 };
 
@@ -16,11 +18,37 @@ Teacher.prototype.toHtml = function(){
     return template(this);
 };
 
+Handlebars.registerHelper('star-icons', function(stars){
+    stars = parseInt(stars);
+    var result = '';
+    switch(stars) {
+        case 5: 
+            result += '&#9733;';
+        case 4:
+            result += '&#9733;';
+        case 3:
+            result += '&#9733;';
+        case 2:
+            result += '&#9733;';
+        default:
+            result += '&#9733;';
+    }
+    switch(stars) {
+        case 1:
+            result += '&#9734;';
+        case 2:
+            result += '&#9734;';
+        case 3:
+            result += '&#9734;';
+        case 4: 
+            result += '&#9734;';
+    }
+    return result;
+});
+
 teachers.forEach(function(teacher){
     teacher_list.push(new Teacher(teacher));
 });
-
-
 teacher_list.forEach(function(teacher){
     $('#teacher-listings').append(teacher.toHtml());
 });
--- js/teachers.js
+++ js/teachers.js
@@ -1,32 +1,37 @@
 var teachers = [
     {
         image: "hauschildt.jpg",
-        name: "Marc Hauschildt",
+        name: "Marc 'The Goat' Hauschildt",
         subjects: ["codeing class", "yearbook", "business law"],
+        stars: 5,
         teacher_id: '001',
     },
     {
         image: "hood.jpg",
-        name: "Betsy Hood",
+        name: "Betsy 'The Chef' Hood",
         subjects: ["child development", "beggening foods", "food and nutrition"],
+        stars: 5,
         teacher_id: '002',
     },
     {
         image: "harvey.jpg",
-        name: "Sarah Harvey",
+        name: "Sarah 'The Math Wiz' Harvey",
         subjects: ["math 2", "math 3", "topics"],
+        stars: 5,
         teacher_id: '003',
     },
     {
         image: "czarnecki.jpg",
-        name: "Mike Czarnecki ",
+        name: "Mike 'The Gym Rat' Czarnecki ",
         subjects: ["gym","strength and conditioning"],
+        stars: 5,
         teacher_id: '004',
     },
     {
         image: "morris.jpg",
-        name: "Zach Morris",
+        name: "Zach 'The Naf' Morris",
         subjects: ["intro to ag","conservation", "ag leadership"],
+        stars: 1,
         teacher_id: '005',
     },
 
